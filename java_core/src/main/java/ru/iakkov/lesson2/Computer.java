990aad236dea00d47682d6e01df169899cef4e99
Iakov Lysenko <lysenko_iakov@yahoo.com>
2025-03-13 22:09:51 +0300

    Модификатор статик

--- java_core/src/main/java/ru/iakkov/lesson2/Computer.java
+++ java_core/src/main/java/ru/iakkov/lesson2/Computer.java
@@ -3,8 +3,18 @@ package ru.iakkov.lesson2;
 public class Computer {
     int ssd = 500;
     int ram = 1024;
+    private static int counter = 0;
 
     void load() {
         System.out.println("Я загрузился");
+    }
+
+    public Computer(int ssd, int ram) {
+        this.ssd = ssd;
+        this.ram = ram;
+        counter++;
+    }
+    int getCounter() {
+        return counter;
     }
 }
--- java_core/src/main/java/ru/iakkov/lesson2/ComputerRunner.java
+++ java_core/src/main/java/ru/iakkov/lesson2/ComputerRunner.java
@@ -1,10 +1,17 @@
 package ru.iakkov.lesson2;
 
+import javax.script.Compilable;
+
 public class ComputerRunner {
     public static void main(String[] args) {
-        Computer computer = new Computer();
+        /* Computer computer = new Computer();
         computer.load();
         System.out.println("RAM: " + computer.ram);
         System.out.println("SSD: " + computer.ssd);
+        */
+        Computer computer1 = new Computer(500, 1024);
+        Computer computer2 = new Computer(600, 2048);
+        System.out.println(computer1.getCounter());
+        System.out.println(computer2.getCounter());
     }
 }
