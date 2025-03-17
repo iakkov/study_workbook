607be9574d9dfa844dd684adece5b9451605a9dd
Iakov Lysenko <lysenko_iakov@yahoo.com>
2025-03-17 21:45:31 +0300

    Коллекции. Map

--- java_core/src/main/java/ru/iakkov/otus/Collections/Cat.java
+++ java_core/src/main/java/ru/iakkov/otus/Collections/Cat.java
@@ -22,4 +22,16 @@ public class Cat {
     public String getInfo() {
         return ("Имя: " + getName() + ", цвет: " + getColor() + ", размер: " + getWight());
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Cat cat = (Cat) o;
+        return Objects.equals(name, cat.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(name);
+    }
 }
---
+++ java_core/src/main/java/ru/iakkov/otus/Collections/DemoMap.java
@@ -0,0 +1,34 @@
+package ru.iakkov.otus.Collections;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class DemoMap {
+    public static void main(String[] args) {
+        Cat murza = new Cat("Мурзик", "серый", 2);
+        Cat vasy = new Cat("Василий", "чёрный", 3);
+        Cat dymka = new Cat("Дымка", "белая", 1);
+
+        HashMap<Cat, Integer> cats = new HashMap<>();
+        cats.put(murza, 1);
+        cats.put(vasy, 2);
+        cats.put(dymka, 3);
+
+        for (Map.Entry<Cat, Integer> cat : cats.entrySet()) {
+            System.out.println(cat);
+        }
+        System.out.println("Можно вывести еще так:");
+        cats.entrySet().forEach(System.out::println);
+
+        int val = cats.get(murza);
+        System.out.println("murza val:" + val);
+
+        murza.setWight(4);
+        int val2 = cats.get(murza);
+        System.out.println("murza val2:" + val2);
+
+        Cat murzaSecond = new Cat(murza.getName(), murza.getColor(), murza.getWight());
+        Integer valSecond = cats.get(murzaSecond);
+        System.out.println("murzaSecond val:" + valSecond);
+    }
+}
