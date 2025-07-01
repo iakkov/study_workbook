23afe4a64fd4a741dd9ffb6f234c11892e507542
iakovlysenko <lysenko_iakov@yahoo.com>
2025-07-01 19:43:30 +0300

    ApplicationContext

--- spring_study/src/main/java/ru/project/spring_study/App.java
+++ spring_study/src/main/java/ru/project/spring_study/App.java
@@ -1,12 +1,14 @@
 package ru.project.spring_study;
 
 import org.springframework.beans.factory.BeanFactory;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class App {
     public static void main(String[] args) {
 
-        BeanFactory factory = new ClassPathXmlApplicationContext("spring.xml");
+        //BeanFactory factory = new ClassPathXmlApplicationContext("spring.xml");
+        ApplicationContext factory = new ClassPathXmlApplicationContext("spring.xml");
 
         Alien obj = factory.getBean("alien", Alien.class);
 
